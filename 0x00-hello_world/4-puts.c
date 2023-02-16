adf64069086d7702e6288843fc35cfdb27326433
sinetebeb <sinetebeb@gmail.com>
2023-02-16 13:44:24 -0800

    0x00. C - Hello, World task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+/**
+ * main - Programming is like building a multilingual puzzle
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle
+");
+return (0);
+}
