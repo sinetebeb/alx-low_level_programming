adb39ebf4245e487ad6bab0e063c9f38168dc3b9
sinetebeb <sinetebeb@gmail.com>
2023-02-16 14:18:34 -0800

    task 4 Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 /**
- * main - Programming is like building a multilingual puzzle
- * Return: 0 (success)
+ * main - My first c hello puts program
+ * Return: 0
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle
-");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
-}
