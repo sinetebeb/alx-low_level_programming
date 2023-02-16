d57898bfc2bf6d74ec79ac2e9ca5a8a8108312d7
sinetebeb <sinetebeb@gmail.com>
2023-02-16 15:49:06 -0800

    0x00. C - Hello, World final

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main -A c program which print a line to standard error
+ * Return 1 (success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r",stdout);
+return (1);
+}
