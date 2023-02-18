d4d7322c82f3ed25739810f371b8e30c7bd65955
sinetebeb <sinetebeb@gmail.com>
2023-02-17 17:34:42 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - A  program that prints all single digit numbers of base 10 starting from 0
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	putchar(d);
+	putchar('\n');
+
+	return (0);
+}
