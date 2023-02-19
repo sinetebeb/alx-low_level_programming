4cae5c26d27702a4a3de5e4461f4fafe7b206d39
sinetebeb <sinetebeb@gmail.com>
2023-02-19 03:50:28 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
  * main - My first c hello puts program
- * Return: 0
+ * Return: 0 (Success)
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
+}
