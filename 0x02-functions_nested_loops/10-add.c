91ab2c58b30089c3aa6ebacdc7d4fddf42814aae
sinetebeb <sinetebeb@gmail.com>
2023-02-22 12:57:49 -0800

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,3 +1,11 @@
+
+/**
+ * main - adds two integers and returns the result
+ * @num1: first integer to add
+ * @num2:second integer to add
+ *
+ * Return: the sum of num1 and num2
+ */
 int add(int num1, int num2)
 {
     int result;
