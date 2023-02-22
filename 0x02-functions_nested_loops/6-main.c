88def987728385d20b710d343cbac9f9aaddea2a
sinetebeb <sinetebeb@gmail.com>
2023-02-22 11:50:11 -0800

    6-abs.c

--- 0x02-functions_nested_loops/6-main.c
+++ 0x02-functions_nested_loops/6-main.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <limits.h>
 
 /**
  * main - check the code
@@ -12,11 +13,27 @@ int main(void)
 
     r = _abs(-1);
     printf("%d\n", r);
+
     r = _abs(0);
     printf("%d\n", r);
+
     r = _abs(1);
     printf("%d\n", r);
+
     r = _abs(-98);
     printf("%d\n", r);
+
+    r = _abs(INT_MAX);
+    printf("%d\n", r);
+
+    r = _abs(INT_MIN);
+    printf("%d\n", r);
+
+    r = _abs(12);
+    printf("%d\n", r);
+
+    r = _abs(0x0F02);
+    printf("%d\n", r);
+
     return (0);
 }
