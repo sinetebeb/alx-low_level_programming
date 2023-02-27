8990bcf06b987c0cbd43fede6fa3fd9763b6787b
sinetebeb <sinetebeb@gmail.com>
2023-02-27 10:15:36 -0800

    I do not fear computers

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string followed by a new line
+ * @str: The string to be print
+ * Return: void
+ */
+
+void _puts(char *str)
+
+{
+	for (; *str != '\0'; str++)
+	{
+	_putchar(*str);
+	}
+	_putchar('\n');
+}
