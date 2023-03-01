df8a06060fda77d6168c42d5709120930df455e2
sinetebeb <sinetebeb@gmail.com>
2023-02-28 18:12:34 -0800

    crack me

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+
+{
+	int pass[100];
+	int i, sum, n;
+
+	sum = 0;	
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
+
+	return (0);
+}
+
