9387789d0f9fb61cc4c1bfcd02f053f99f85fbb9
sinetebeb <sinetebeb@gmail.com>
2023-04-04 04:14:19 -0700

    4. Free list update

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -9,9 +9,9 @@ void free_listint(listint_t *head)
 	listint_t *temp;
 
 	while (head)
-	 {
-		 temp = head->next;
-		 free(head);
-		 head = temp;
-	 }
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
 }
