db08267e4c5455ef845a87274617705c9ab35091
sinetebeb <sinetebeb@gmail.com>
2023-04-04 04:21:15 -0700

    1. List length update

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -10,11 +10,11 @@ size_t listint_len(const listint_t *h)
 {
 	size_t num = 0;
 
-	 while (h)
-	 {
-		 num++;
-		 h = h->next;
-	 }
+	while (h)
+	{
+		num++;
+		h = h->next;
+	}
 
-	 return (num);
+return (num);
 }
