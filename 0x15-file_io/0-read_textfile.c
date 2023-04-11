cce7b2e4de446739a091001d49ab6d4b3afe7d6e
sinetebeb <sinetebeb@gmail.com>
2023-04-10 18:10:17 -0700

    update3

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,6 +18,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
+
 	buf = malloc(sizeof(char) * letters);
 	t = read(fd, buf, letters);
 	w = write(STDOUT_FILENO, buf, t);
